43954efd4a41b25f85e70a7d2a7d719449059d4b
supericeliu <supericeliu@tencent.com>
2023-07-26 10:44:38 +0800

    fix potential bug if send message to child thread before its message queue built.

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const setClipboardListener = require('./build/Release/clipboard-event-windows.node')
+const setClipboardListener = require('./build/Debug/clipboard-event-windows.node')
 console.log(setClipboardListener);
 let count = 0
 
 setClipboardListener(() => {
     console.log('update');
-    // setClipboardListener();
-    // setClipboardListener(() => {
-    //     console.log('foo')
-    // })
+});
+
+setClipboardListener();
+
+setClipboardListener(() => {
+    console.log('foo');
 });
 
 globalThis.setClipboardListener = setClipboardListener;
-
-setInterval(() => {
-
-}, 1000);
